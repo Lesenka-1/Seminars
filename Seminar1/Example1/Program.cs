c436f3733f81bac0899eaa31258ecba5a0377bca
alesia <albenderina@mail.ru>
2023-01-15 22:40:21 +0300

    Разобрали Пример

---
+++ Seminar1/Example1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат 
+
+Console.Write ("Введите чило: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number*number;
+
+//Вывод в лоб
+Console.Write("Квадрат числа ");
+Console.Write(number);
+Console.Write(" равен ");
+Console.WriteLine(result);
+//Вывод с $
+Console.WriteLine($"Квадрат числа {number} равен {result}");
+//Вывод с обращением к переменным
+Console.WriteLine("Квадрат числа {0} равен {1}", number, result);
+
+int result1 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine("Квадрат числа {0} равен {1}", number, result);
+
+Console.WriteLine("Квадрат числа " + number + " равен " + result1);
